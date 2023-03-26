adde1a0e52ca192bfd9098a3457d970210bfa500
Akshay More <119856540+Akshay-More-007@users.noreply.github.com>
2023-03-26 21:41:11 +0530

    adding all required deployment files to git

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
+import json
+
+# Load your trained model here
+model = joblib.load('D:\Bank_Churn_Prediction\model\model.pkl')
+
+feat_cols = ['CreditScore', 'Geography', 'Gender', 'Age', 'Tenure', 'Balance', 'NumOfProducts',
+             'HasCrCard', 'IsActiveMember', 'EstimatedSalary']
+
+context_dict = {
+    'feats': feat_cols,
+    'zip': zip,
+    'range': range,
+    'len': len,
+    'list': list,
+}
+
+# Initiate the application
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def predict_churn():
+    if request.method == 'POST':
+        CreditScore = (request.form['CreditScore'])
+        Geography = (request.form['Geography'])
+        Gender = (request.form['Gender'])
+        Age = (request.form['Age'])
+        Tenure = (request.form['Tenure'])
+        Balance = (request.form['Balance'])
+        NumOfProducts = (request.form['NumOfProducts'])
+        HasCrCard = (request.form['HasCrCard'])
+        IsActiveMember = (request.form['IsActiveMember'])
+        EstimatedSalary = (request.form['EstimatedSalary'])
+
+        X = np.array([[CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary]])
+
+        # Make prediction
+        pred = model.predict(X)
+        print('Prediction:', pred)
+
+
+        # Return prediction as JSON response
+        return json.dumps({'prediction': pred.tolist()})
+    else:
+        # Render the template with input form
+        return render_template('index.html', **context_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,17 @@
+import pickle
+import pandas as pd
+import numpy as np
+import os
+
+curr_path = os.path.dirname(os.path.realpath(__file__))
+model_path = os.path.join(curr_path, + 'model\model.pkl')
+model = pickle.load(open(model_path, 'rb'))
+
+def predict_churn(attributes: np.ndarray):
+    """ Returns 1 if customer is likely to churn, 0 otherwise"""
+    # print(attributes.shape) # (1,10)
+
+    pred = model.predict(attributes)
+    print("Churn predicted")
+
+    return int(pred[0])
